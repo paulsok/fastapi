98cb093fa2a80ffac530466644229c4a222bee8b
Pavel Sokolov <paul.s.falke@gmail.com>
2022-08-03 00:11:23 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException, status, Depends
+
+development_db = ["DB for Development"]
+
+def get_db_session():
+    return development_db
+
+app = FastAPI()
+
+
+@app.get("/add-item/")
+def add_item(item:str, db = Depends(get_db_session)):
+    db.append(item)
+    print(db)
+    return {"message":f"added item {item}"}
