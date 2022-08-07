5c1bedeb39b92953873d0d2044c1eacef57fcf3b
Pavel Sokolov <paul.s.falke@gmail.com>
2022-08-07 21:18:50 +0500

    feat: dependencies

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import FastAPI, Depends
 
-development_db = ["DB for Development"]
-
-def get_db_session():
-    return development_db
 
 app = FastAPI()
 
+dummy_db = {
+    '1': "SDE1 at Google",
+    '2': "SDE2 at Amazon",
+    '3': "Backend Dev. at Spotify",
+    '4': "Senior Engineer at Alphabet",
+    '5': "Devops Eng. at Microsoft",
+}
 
-@app.get("/add-item/")
-def add_item(item:str, db = Depends(get_db_session)):
-    db.append(item)
-    print(db)
-    return {"message":f"added item {item}"}
+class GetObjectOrFeatured:
+    """We let companies promote their jobs. The promoted job
+    is called featured_job"""
+
+    def __init__(self, featured_job: str):
+        self.featured_job = featured_job
+
+    def __call__(self, id: str) -> str:
+        value = dummy_db.get(id)
+        if not value:
+            value = dummy_db.get(self.featured_job)
+        return value
+
+
+@app.get("/job/{id}")
+def get_job_by_id(job_title: str = Depends(GetObjectOrFeatured('2'))):
+    return job_title
