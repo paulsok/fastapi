2402c2ef01e261711710bff73457e6f01b685a4e
Pavel Sokolov <paul.s.falke@gmail.com>
2022-08-15 23:18:14 +0300

    feat: add logo to main page

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles #new
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
-
-
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
 
 def include_router(app):
     app.include_router(general_pages_router)
 
 
-def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
-    include_router(app)
-    return app
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_static(app)
+    return app
 
 app = start_application()
