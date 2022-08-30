51b70808d5466647449bf756858003cbaac636e2
--replace-all <paul.s.falke@gmail.com>
2022-08-31 01:05:44 +0300

    refactor: main.py users.py jobs.py

--- backend/db/models/jobs.py
+++ backend/db/models/jobs.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean,Date, ForeignKey
 from sqlalchemy.orm import relationship
-
 from db.base_class import Base
 
 
