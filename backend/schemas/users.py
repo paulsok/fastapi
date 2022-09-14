a2bcabcc05d86c04f0bc5b5f5993458b79b53161
--replace-all <paul.s.falke@gmail.com>
2022-09-14 22:06:14 +0200

    feature: routes are added

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,9 +1,18 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel,EmailStr
 
 
 # properties required during user creation
 class UserCreate(BaseModel):
     username: str
-    email : EmailStr
-    password : str
+    email: EmailStr
+    password: str
+
+
+class ShowUser(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        orm_mode = True
