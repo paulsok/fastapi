b2903052fe1db16e706c8c9c53b3c3fc3a154051
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2022-11-06 20:08:32 +0100

    test: user token headers

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,10 +4,13 @@ from typing import Any
 from typing import Generator
 
 import pytest
+from core.config import settings
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
+from tests.utils.users import authentication_token_from_email
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from db.base import Base
@@ -69,3 +72,10 @@ def client(
     app.dependency_overrides[get_db] = _get_test_db
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture(scope="module")
+def normal_user_token_headers(client: TestClient, db_session: Session):
+    return  authentication_token_from_email(
+        client=client, email=settings.TEST_USER_EMAIL, db=db_session
+    )
--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -3,16 +3,16 @@ import json
 from fastapi import status
 
 
-def test_create_job(client):
+def test_create_job(client, normal_user_token_headers):
     data = {
         "title": "SDE super",
         "company": "doogle",
         "company_url": "www.doogle.com",
         "location": "USA,NY",
         "description": "python",
-        "date_posted": "2022-03-20"
-        }
-    response = client.post("/jobs/create-job/", json.dumps(data))
+        "date_posted": "2022-03-20",
+    }
+    response = client.post("/jobs/create-job/", data=json.dumps(data), headers=normal_user_token_headers)
     assert response.status_code == 200
     assert response.json()["company"] == "doogle"
     assert response.json()["description"] == "python"
